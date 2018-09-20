bb5ebacdeabf0fa9c98321ab6977c05ce7f841c7
Tyler Fisher <tfisher@politico.com>
2018-09-20 11:48:09 -0400

    0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="politico-civic-ap-loader",
-    version="0.1.0",
+    version="0.1.1",
     description="",
     url="https://github.com/The-Politico/politico-civic-ap-loader",
     author="POLITICO interactive news",
